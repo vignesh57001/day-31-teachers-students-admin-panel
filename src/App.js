17b27c1af3faa4cc9f9a4d4e4e51e9f16ba3fd3b
vignesh57001 <109303622+vignesh57001@users.noreply.github.com>
2023-06-17 01:30:01 +0530

    new file

--- src/App.js
+++ src/App.js
@@ -23,7 +23,6 @@ function App() {
           <div id="content">
             <Topbar />
             <Routes>
-              <Route path="/" element={<Teachers />}></Route>
               <Route path="/dashboard" element={<Dashboard />}></Route>
               <Route path="/teachers" element={<Teachers />}></Route>
               <Route path="/students" element={<Students />}></Route>
